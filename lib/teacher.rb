d73977b3ebcfa851c22b8ba4acf6213b562ded04
Anysia V <anysiavalko@gmail.com>
2019-01-16 18:24:59 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < User
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  KNOWLEDGE = ["junior mints are delicious", "starbursts will break your teeth", "skittles really should have a blue flavor"]
 
   def teach 
     KNOWLEDGE.sample
