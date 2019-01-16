95551dad84bfd48147b7a4c25aa702619302aea0
Anysia V <anysiavalko@gmail.com>
2019-01-16 18:35:04 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < User
 
-  KNOWLEDGE = ["junior mints are delicious", "starbursts will break your teeth", "skittles really should have a blue flavor"]
+  KNOWLEDGE = ["junior Mints are delicious", "Starbursts will break your teeth", "Skittles really should have a blue flavor", "Nerds are just not worth the trouble", "Red Vines are better than Twizzlers"]
 
   def teach 
     KNOWLEDGE.sample
