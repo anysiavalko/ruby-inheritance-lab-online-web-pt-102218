1dd5fea9e1725bcce1e59fca62da0058d90ecdcc
Anysia V <anysiavalko@gmail.com>
2019-01-16 18:41:59 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < User
 
-  KNOWLEDGE = ["junior Mints are delicious", "Starbursts will break your teeth", "Skittles really should have a blue flavor", "Nerds are just not worth the trouble", "Red Vines are better than Twizzlers"]
+  KNOWLEDGE = ["Junior Mints are delicious", "Starbursts will break your teeth", "Skittles aren't the rainbow if they don't have blue", "Nerds are just not worth the trouble of spilling them everywhere", "Red Vines are better than Twizzlers", "Hershey Kisses aren't worth the trouble of opening the wrapper", "Cotton Candy is disgusting", "How is Baby Ruth still being made?", "Swedish Fish are addicting", "Kit Kats never get old", "Almond Joy over Mounds every time", "York Peppermint patties are yum" ]
 
   def teach 
     KNOWLEDGE.sample
